dc469d02a8fd251f48e47707627e145b4eaeab54
jessicaboahene036 <jessicaboahene036@gmail.com>
2026-06-09 12:59:15 +0000

    Add files via upload

---
+++ work2.java
@@ -0,0 +1,10 @@
+public class work2 {
+    public static void main(String[] arg){
+        System.out.println("X                         X");
+        System.out.println("X                         X");
+        System.out.println("X        XXXXXXXXXX       X");
+        System.out.println("XXXXX    X        X   XXXXX");
+        System.out.println("X   X    X        X   X   X");
+        System.out.println("X   X    X        X   X   X");
+    }
+}
